3da794c7d2c211a83b9f81a373e31ce37194e0db
Samuel Moelius <sam@moeli.us>
2025-06-05 05:36:54 -0400

    Rerun if anything in the `out_dir` changes

--- build.rs
+++ build.rs
@@ -1,18 +1,10 @@
-use std::{
-    env::var,
-    fs::{read_to_string, write},
-    path::PathBuf,
-    time::Instant,
-};
+use std::{env::var, fs::write, path::Path, time::Instant};
 
 fn main() {
     let out_dir = var("OUT_DIR").unwrap();
-    let path = PathBuf::from(out_dir).join("now.txt");
+    let path = Path::new(&out_dir).join("now.txt");
     let now = format!("{:?}", Instant::now());
-    if path.exists() {
-        let before = read_to_string(&path).unwrap();
-        assert_eq!(before, now);
-    }
+    assert!(!path.exists());
     write(&path, now).unwrap();
-    println!("cargo::rerun-if-changed={}", path.display());
+    println!("cargo::rerun-if-changed={out_dir}");
 }
